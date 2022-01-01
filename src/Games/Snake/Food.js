ac26eb2f6f2eaa820d5dd7979aab2790ace03296
Arshpreet Chabbewal <arsh.c321@gmail.com>
2021-12-31 23:04:55 -0500

    finished snake game

--- src/Games/Snake/Food.js
+++ src/Games/Snake/Food.js
@@ -4,8 +4,9 @@ import "./GameArea.css";
 const Food = ({ dot }) => {
   const style = {
     left: `${dot[0]}%`,
-    top: `${dot[0]}%`,
+    top: `${dot[1]}%`,
   };
+  console.log(`FOOD: ${dot}`);
   return (
     <div className="snake-food" style={style}>
       <div></div>
--- src/Games/Snake/GameArea.js
+++ src/Games/Snake/GameArea.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react";
 import Snake from "./Snake";
 import Food from "./Food";
 import "./GameArea.css";
@@ -10,6 +10,18 @@ const GameArea = () => {
     [4, 0],
   ]);
 
+  const [direction, setDirection] = useState("RIGHT");
+  const [food, setFood] = useState(null);
+  const [speed, setSpeed] = useState(200);
+
+  const snakeDotsRef = useRef(null);
+  const directionRef = useRef(null);
+  const foodRef = useRef(null);
+
+  snakeDotsRef.current = snakeDots;
+  directionRef.current = direction;
+  foodRef.current = food;
+
   const getRandomCoordinates = () => {
     let min = 1;
     let max = 98;
@@ -18,12 +30,121 @@ const GameArea = () => {
     return [x, y];
   };
 
-  const [food, setFood] = useState(null);
   if (food === null) {
     const coordinates = getRandomCoordinates();
-    console.log(coordinates);
     setFood(coordinates);
   }
+
+  const moveSnake = () => {
+    let dots = [...snakeDotsRef.current];
+    let head = dots[dots.length - 1];
+
+    switch (directionRef.current) {
+      case "RIGHT":
+        head = [head[0] + 2, head[1]];
+        break;
+      case "LEFT":
+        head = [head[0] - 2, head[1]];
+        break;
+      case "UP":
+        head = [head[0], head[1] - 2];
+        break;
+      case "DOWN":
+        head = [head[0], head[1] + 2];
+        break;
+    }
+
+    dots.push(head);
+    dots.shift();
+    setSnakeDots(dots);
+  };
+
+  const escFunction = useCallback((event) => {
+    event = event || window.event;
+    switch (event.keyCode) {
+      case 38:
+        setDirection("UP");
+        break;
+      case 40:
+        setDirection("DOWN");
+        break;
+      case 37:
+        setDirection("LEFT");
+        break;
+      case 39:
+        setDirection("RIGHT");
+        break;
+    }
+  }, []);
+
+  const checkIfOutOfBorders = () => {
+    let head = snakeDots[snakeDots.length - 1];
+    if (head[0] >= 100 || head[1] >= 100 || head[0] < 0 || head[1] < 0) {
+      onGameOver();
+    }
+    return false;
+  };
+
+  const checkIfCollapsed = () => {
+    let snake = [...snakeDots];
+    let head = snake[snake.length - 1];
+    snake.pop();
+    snake.forEach((dot) => {
+      if (head[0] === dot[0] && head[1] === dot[1]) {
+        onGameOver();
+      }
+    });
+  };
+
+  const checkIfEat = () => {
+    let head = snakeDotsRef.current[snakeDotsRef.current.length - 1];
+    console.log(food);
+    console.log(`HEAD: ${head}`);
+    if (head[0] === food[0] && head[1] === food[1]) {
+      setFood(getRandomCoordinates());
+      enlargeSnake();
+      increaseSpeed();
+    }
+  };
+
+  const enlargeSnake = () => {
+    let newSnake = [...snakeDots];
+    newSnake.unshift([]);
+    setSnakeDots(newSnake);
+  };
+
+  const increaseSpeed = () => {
+    if (speed > 10) {
+      setSpeed(speed - 10);
+    }
+  };
+
+  const onGameOver = () => {
+    alert(`Game Over. Snake length is ${snakeDots.length}`);
+    setDirection("RIGHT");
+    setSnakeDots([
+      [0, 0],
+      [2, 0],
+      [4, 0],
+    ]);
+    setFood(null);
+    setSpeed(200);
+  };
+
+  useEffect(() => {
+    setInterval(moveSnake, speed);
+    document.addEventListener("keydown", escFunction, false);
+
+    return () => {
+      document.removeEventListener("keydown", escFunction, false);
+    };
+  }, []);
+
+  useEffect(() => {
+    checkIfOutOfBorders();
+    checkIfCollapsed();
+    checkIfEat();
+  }, [snakeDots]);
 
   return (
     <div className="game-area">
